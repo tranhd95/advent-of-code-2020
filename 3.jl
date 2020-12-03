792ac02a89beeb54de1db936a4eb73a3f2ed3a36
Hai Duong Tran <tranhd95@gmail.com>
2020-12-03 10:29:29 +0100

    Add day 3

---
+++ 3.jl
@@ -0,0 +1,28 @@
+pattern = readlines("files/3.txt")
+
+function part1(pattern, slope)
+    row, col = 1, 1
+    row_length = length(pattern[begin])
+    trees_count = 0
+    while row ≤ length(pattern)
+        trees_count += pattern[row][col] == '#'
+        col = ((col + slope[1]) > row_length) ? (col + slope[1]) - row_length : col + slope[1] 
+        row += slope[2]
+    end
+    trees_count
+end
+
+slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2]
+]
+    
+function part2(pattern)
+    reduce(*, map(slope -> part1(pattern, slope), slopes))
+end
+    
+println(part2(pattern))
+println(part1(pattern, [3, 1]))
