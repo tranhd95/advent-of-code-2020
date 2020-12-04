2a23142c3b2720496ac67a70cea04b4052bedccb
Hai Duong Tran <tranhd95@gmail.com>
2020-12-04 10:37:24 +0100

    Add day4

---
+++ 4.jl
@@ -0,0 +1,54 @@
+LINES = readlines("files/4.txt")
+PASSPORTS = split(join(LINES, "-"), "--")
+
+function part1(passports)
+    count(is_valid, passports)
+end
+
+function is_valid(passport)
+    required = Set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
+    pairs = get_pairs(passport)
+    keys = Set(map(pair -> pair[1], pairs))
+    required ⊆ keys
+end
+
+function get_pairs(passport)
+    hyphen_removed = replace(passport, "-" => " ")
+    pairs_with_semicolon = split(hyphen_removed, " ")
+    split.(pairs_with_semicolon, ":")
+end
+
+function part2(passports)
+    count(is_valid2, passports)
+end
+
+function is_valid2(passport)
+    required = Set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
+    pairs = get_pairs(passport)
+    keys = Set(map(pair -> pair[1], pairs))
+    required ⊆ keys && all(is_valid_pair, pairs)
+end
+
+function is_valid_pair(pair)
+    key, value = pair
+    rules = Dict(
+        "byr" => value -> "1920" ≤ value ≤ "2002",
+        "iyr" => value -> "2010" ≤ value ≤ "2020",
+        "eyr" => value -> "2020" ≤ value ≤ "2030",
+        "hgt" => value -> ((endswith(value, "cm") && "150" ≤ value[begin:end - 2] ≤ "193") || 
+                         (endswith(value, "in") && "59" ≤ value[begin:end - 2] ≤ "76")
+                        ),
+        "hcl" => value -> startswith(value, "#") && 
+                        length(value[begin + 1:end]) == 6 &&
+                        all(symbol -> occursin(symbol, "0123456789abcdef"), value[begin + 1:end]),
+        "ecl" => value -> value ∈ ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+        "pid" => value -> length(value) == 9 && 
+                        all(digit -> occursin(digit, "0123456789"), value),
+        "cid" => _ -> true
+    )
+    return rules[key](value)
+end
+
+
+println(part1(PASSPORTS))
+println(part2(PASSPORTS))
