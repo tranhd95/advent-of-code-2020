146f26693748854213ba5ce397a05dce224bb85e
Hai Duong Tran <tranhd95@gmail.com>
2020-12-06 10:18:11 +0100

    Clean

--- 6.jl
+++ 6.jl
@@ -2,13 +2,8 @@ LINES = map(strip, readlines("files/6.in"))
 ANSWERS = split(join(LINES, "-"), "--")
 
 # Part 1
-# sum(ANSWERS .|> Set .|> set -> delete!(set, '-') |> length)
+sum(ANSWERS .|> Set .|> set -> delete!(set, '-') |> length) |> println
 
 # Part 2
 groups = ANSWERS .|> a -> split(a, '-')
-global summ = 0
-for group in groups
-    sets = group .|> Set
-    global summ += intersect(sets...) |> length
-end
-summ
+mapreduce(group -> intersect((group |> Set)...) |> length, +, groups) |> println
