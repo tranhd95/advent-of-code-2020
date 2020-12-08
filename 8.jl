e9f3e0a583873d6adee2bc2d79ab8f5525ab17aa
Hai Duong Tran <tranhd95@gmail.com>
2020-12-08 11:05:52 +0100

    Revert

--- 8.jl
+++ 8.jl
@@ -1,50 +1,47 @@
-@time begin
-    INSTRUCTIONS = split.(readlines("files/8.in"), " ") .|> 
-                      x -> [x[1], parse(Int, x[2])]
+INSTRUCTIONS = split.(readlines("files/8.in"), " ")
 
-    function does_terminate(instructions)
-        acc = 0
-        executed = Set()
-        l = 1
-        while l ≤ length(instructions)
-            if l ∈ executed
-                return acc, false
-            end
-            push!(executed, l)
-            ins, arg = instructions[l][1], instructions[l][2]
-            if ins == "nop"
-                l += 1
-            elseif ins == "acc"
-                acc += arg
-                l += 1
-            elseif ins == "jmp"
-                l += arg
-            end
+function does_terminate(instructions)
+    acc = 0
+    executed = Set()
+    l = 1
+    while l ≤ length(instructions)
+        if l ∈ executed
+            return acc, false
         end
-        return acc, true
-    end
-
-    function part1(instructions)
-        does_terminate(instructions)
-    end
-
-    function part2(instructions)
-        for (i, (ins, arg)) ∈ enumerate(instructions)
-            if ins == "nop"
-                instructions[i] = ["jmp", arg]
-            elseif ins == "jmp"
-                instructions[i] = ["nop", arg]
-            end
-            acc, isTerminated = does_terminate(instructions)
-            if isTerminated
-                return acc
-            end
-            instructions[i] = [ins, arg]
+        push!(executed, l)
+        ins, arg = instructions[l][1], parse(Int, instructions[l][2])
+        if ins == "nop"
+            l += 1
+        elseif ins == "acc"
+            acc += arg
+            l += 1
+        elseif ins == "jmp"
+            l += arg
         end
     end
-
-
-
-    part1(INSTRUCTIONS) |> first |> println
-    part2(INSTRUCTIONS) |> println
+    return acc, true
 end
+
+function part1(instructions)
+    does_terminate(instructions)
+end
+
+function part2(instructions)
+    for (i, (ins, arg)) ∈ enumerate(instructions)
+        if ins == "nop"
+            instructions[i] = ["jmp", arg]
+        elseif ins == "jmp"
+            instructions[i] = ["nop", arg]
+        end
+        acc, isTerminated = does_terminate(instructions)
+        if isTerminated
+            return acc
+        end
+        instructions[i] = [ins, arg]
+    end
+end
+
+
+
+part1(INSTRUCTIONS) |> first |> println
+part2(INSTRUCTIONS) |> println
