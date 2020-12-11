826eadec5aafb647ed25c477876e7af551f987b5
Hai Duong Tran <tranhd95@gmail.com>
2020-12-11 12:55:48 +0100

    Lil cleanup

--- 11.jl
+++ 11.jl
@@ -1,85 +1,71 @@
-INPUT = readlines("files/11.in") .|> str -> split(str, "")
-
 function part1(input)
-    prev = deepcopy(input)
-    new = apply_rules(deepcopy(input))
+    prev = []
+    new = apply_rules(input)
+    cnt_applications = 1
     while prev != new
-        prev = deepcopy(new)
+        prev = new
         new = apply_rules(new)
-        # joint = new .|> line -> join(line, "") 
-        # show(stdout, "text/plain", joint)
-        # println()
+        cnt_applications += 1
     end
-    count.(el -> el == '#', join.(new, "")) |> sum
+    count.(el -> el == '#', join.(new, "")) |> sum, cnt_applications
 end
 
 function apply_rules(map)
     result = deepcopy(map)
     width, height = length(map[begin]), length(map)
-    for row ∈ 1:height 
-        for col ∈ 1:width
-            if map[row][col] == "."
-                continue
-            end
-            neigbours = count_neighbours(map, row, col)
-            # print(neigbours)
-            if map[row][col] == "L" && neigbours == 0
-                result[row][col] = "#"
-            elseif map[row][col] == "#" && neigbours ≥ 4
-                result[row][col] = "L"
-            end
+    for row ∈ 1:height, col ∈ 1:width
+        if map[row][col] == "."
+            continue
         end
-        # println()
+        neigbours = count_neighbours(map, row, col)
+        if map[row][col] == "L" && neigbours == 0
+            result[row][col] = "#"
+        elseif map[row][col] == "#" && neigbours ≥ 4
+            result[row][col] = "L"
+        end
     end
     result
 end
 
 function count_neighbours(map, row, col)
     cnt = 0
+    width, height = length(map[1]), length(map)
     for y ∈ row - 1:row + 1, x ∈ col - 1:col + 1
-        if y == row && x == col
+        if (y == row && x == col)
             continue
         end
-        try
-            if map[y][x] == "#"
-                cnt += 1
-            end
-        catch
+        if (1 ≤ y ≤ height && 1 ≤ x ≤ width) && map[y][x] == "#"
+            cnt += 1
         end
     end
     cnt
 end
 
 function part2(input)
-    prev = deepcopy(input)
-    new = apply_rules(deepcopy(input))
+    prev = input
+    new = apply_rules(input)
+    cnt_applications = 1
     while prev != new
-        prev = deepcopy(new)
+        prev = new
         new = apply_rules2(new)
-        # joint = new .|> line -> join(line, "") 
-        # show(stdout, "text/plain", joint)
-        # println()
+        cnt_applications += 1
     end
-    count.(el -> el == '#', join.(new, "")) |> sum
+    count.(el -> el == '#', join.(new, "")) |> sum, cnt_applications
 end
 
 function apply_rules2(map)
     result = deepcopy(map)
     width, height = length(map[begin]), length(map)
-    for row ∈ 1:height 
-        for col ∈ 1:width
-            if map[row][col] == "."
-                continue
-            end
-            neigbours = count_neighbours2(map, row, col)
-            # print(neigbours)
-            if map[row][col] == "L" && neigbours == 0
-                result[row][col] = "#"
-            elseif map[row][col] == "#" && neigbours ≥ 5
-                result[row][col] = "L"
-            end
+    for row ∈ 1:height, col ∈ 1:width
+        if map[row][col] == "."
+            continue
         end
-        # println()
+        neigbours = count_neighbours2(map, row, col)
+        if map[row][col] == "L" && neigbours == 0
+            result[row][col] = "#"
+        elseif map[row][col] == "#" && neigbours ≥ 5
+            result[row][col] = "L"
+        end
     end
     result
 end
@@ -110,5 +96,8 @@ function count_neighbours2(map, row, col)
     cnt
 end
 
-# part1(INPUT)           
-part2(INPUT)             
+@time begin
+    INPUT = readlines("files/11.in") .|> str -> split(str, "")
+    part1(INPUT) |> println 
+    part2(INPUT) |> println
+end
